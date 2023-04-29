a4233c8d7e51d577b9f8fc7a4ddbfd6f3c2a9d64
Devin <devink12110@gmail.com>
2023-04-29 12:47:01 -0400

    Header styled

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './css/style.css';
+import './css/index.css';
+
 export const contentDiv = document.getElementById('content');
 export default function createEl(type) {
   return document.createElement(type);
@@ -7,6 +10,7 @@ const header = createEl('header');
 const h1 = createEl('h1');
 const ul = createEl('ul');
 const btn = createEl('button');
+const btnCont = createEl('div');
 
 for (let i = 0; i < 3; i += 1) {
   const arr = ['HOME', 'MENU', 'ABOUT US'];
@@ -15,10 +19,13 @@ for (let i = 0; i < 3; i += 1) {
   ul.appendChild(li);
 }
 
+btnCont.classList.add('btn-cont');
+
 h1.textContent = `COFFEE O' CLOCK`;
 btn.textContent = 'CONTACT US';
 
+btnCont.appendChild(btn);
 header.appendChild(h1);
 header.appendChild(ul);
-header.appendChild(btn);
+header.appendChild(btnCont);
 contentDiv.appendChild(header);
