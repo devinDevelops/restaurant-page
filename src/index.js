14191cb1a4b3214b910ab8c870ddf113cc142bb8
Devin <devink12110@gmail.com>
2023-04-29 12:57:30 -0400

    Adjust header h1
    When the browser window size was increased, the spacing of the header was off.
    
    Added a wrapper to the h1 to add flex properties.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export default function createEl(type) {
 
 const header = createEl('header');
 const h1 = createEl('h1');
+const h1Cont = createEl('div');
 const ul = createEl('ul');
 const btn = createEl('button');
 const btnCont = createEl('div');
@@ -19,13 +20,15 @@ for (let i = 0; i < 3; i += 1) {
   ul.appendChild(li);
 }
 
+h1Cont.classList.add('h1-cont');
 btnCont.classList.add('btn-cont');
 
 h1.textContent = `COFFEE O' CLOCK`;
 btn.textContent = 'CONTACT US';
 
+h1Cont.appendChild(h1);
 btnCont.appendChild(btn);
-header.appendChild(h1);
+header.appendChild(h1Cont);
 header.appendChild(ul);
 header.appendChild(btnCont);
 contentDiv.appendChild(header);
