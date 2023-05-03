bd32b1e160efdaff8847ad76f51390162e42a108
Devin <devink12110@gmail.com>
2023-05-03 09:49:45 -0400

    Load page fn set up for index listener fn
    In this iteration, loadPage fn is set up to take e.target.textContent from index.js as an argument and return the corresponding main el.

--- src/js/loadPage.js
+++ src/js/loadPage.js
@@ -1,20 +1,30 @@
 import '../css/style.css';
 import createHeader from './components/header';
 import createFooter from './components/footer';
-// import createMenu from './components/main-components/menuMain';
+import createMenu from './components/main-components/menuMain';
 import createHome from './components/main-components/homeMain';
-// import createAboutUs from './components/main-components/aboutUsMain';
+import createAboutUs from './components/main-components/aboutUsMain';
 
-export default function loadPage() {
+export default function loadPage(tab) {
+  let mainToDisp;
   const contentDiv = document.getElementById('content');
   function createEl(type) {
     return document.createElement(type);
   }
 
+  function pickMain() {
+    if (tab === 'HOME') mainToDisp = createHome(createEl, contentDiv);
+
+    if (tab === 'MENU') mainToDisp = createMenu(createEl, contentDiv);
+
+    if (tab === 'ABOUT US') mainToDisp = createAboutUs(createEl, contentDiv);
+
+    return mainToDisp();
+  }
+
   createHeader(createEl, contentDiv);
-  // createAboutUs(createEl, contentDiv);
-  // createMenu(createEl, contentDiv);
   createHome(createEl, contentDiv);
+  pickMain();
   // have this part be the argument that is passed in when calling loadPage
   createFooter(createEl, contentDiv);
 }
